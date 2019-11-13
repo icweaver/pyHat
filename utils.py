277f63d7af12caa51ee575c58db20e0b82a23d76
Tanveer <tanveer.karim@cfa.harvard.edu>
2019-11-13 15:19:56 -0500

    add rank func

--- utils.py
+++ utils.py
@@ -1,12 +1,9 @@
-def z_scale(x):
-	"""Computes the rank normalization of an array.
+def rank(x):
+	"""computes ranks for a 1D array using
+	the average method to break ties."""
+	r = pd.DataFrame(x).rank().values[:,0] 
 	
-	Parameters
-	----------
-	x : np.array
-		numpy array
-		
-	Returns:
-	z : np. array"""
-	
-	
+	if(len(x) != 0): #make sure that x has at least 1 element
+		return r
+	else:
+		raise Exception("You gave a 0 length array.")
