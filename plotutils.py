440b7cf623ebeb98ad868bf7daaa87a1226df8db
Tanveer <tanveer.karim@cfa.harvard.edu>
2019-11-14 22:30:29 -0500

    updated rankplot to include chains

--- plotutils.py
+++ plotutils.py
@@ -1,7 +1,22 @@
 from utils import *
 import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
 
-def rankplot(x):
-	"""Returns rank plot for a given chain."""
-	x_scaled = rank(x)
-	plt.hist(x_scaled, alpha = 0.8, bins = 50, histtype = 'bar', ec='black')
+def rankplot(trace, nchains, rows, cols):
+	"""Returns rank plot for a given chain.
+	Parameters
+	----------
+	trace : numpy array of trace.
+	nchains : number of chains to be plotted; nchains = rows * cols
+	rows : number of rows in the plot
+	cols: number of cols in the plot
+	"""
+	
+	fig = plt.figure(figsize=(17, 10))
+	gs = GridSpec(rows, cols)
+
+	for i in range(nchains):
+		ax = fig.add_subplot(gs[i])
+		trace_scaled = rank(trace[i])
+		plt.hist(trace_scaled, alpha = 0.8, bins = 50, histtype = 'bar', ec='black')
+		ax.set_title(f"Chain : {i + 1}")
